23eaf0f3307ec7f6e6548718d7130e1b568e7f1c
Samuel <samudawb@gmail.com>
2024-08-21 14:00:21 +0100

    Rendering Animals 2 - Operador ternario

--- my-first-react-app/my-first-react-app/src/App.jsx
+++ my-first-react-app/my-first-react-app/src/App.jsx
@@ -3,14 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function ListItem(props) {
-  return <li>{props.animal}</li>
+// function ListItem(props) {
+//   return <li>{props.animal}</li>
+// }
+
+function List(props){
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        //return <ListItem key={animal} animal={animal} />
+        return animal.startsWith("L") && <li key={animal}>{animal}</li>
+      })}
+    </ul>
+  );
 }
 
 function App() {
   const animals = ["Lion", "Cow", "Snake", "Lizard"];
   //Another way but same
-  const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>)
+  //const animalsList = animals.map((animal) => <li key={animal}>{animal}</li>)
 
   return (
     // <div>
@@ -24,12 +35,7 @@ function App() {
     // </div>
     <div>
       <h1>Animals: </h1>
-      <ul>
-        {/* {animals.map((animal) => {
-          return <li key={animal}>{animal}</li>;
-        })} */
-        animalsList}
-      </ul>
+        <List animals={animals} />
     </div>
   )
 }
